12cc99d1ea74e9dda8c1bbbe39cd9daf302426f4
Leo <yh821@qq.com>
2018-10-24 21:38:41 +0800

    update

---
+++ iosapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosapp
+//
+//  Created by yuanhuan on 2018/10/24.
+//  Copyright © 2018年 yuanhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosapp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  iosapp
+//
+//  Created by yuanhuan on 2018/10/24.
+//  Copyright © 2018年 yuanhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
