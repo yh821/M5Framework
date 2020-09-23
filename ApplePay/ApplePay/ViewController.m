29bf165c1bb9d5d81819924ec0ebc1aee0b5ef42
Leo <m5pro@M1-MacPro.local>
2020-09-23 15:33:58 +0800

    leo

--- ApplePay/ApplePay/ViewController.m
+++ ApplePay/ApplePay/ViewController.m
@@ -137,7 +137,8 @@
 
 #pragma mark - Touch ID
 - (IBAction)onClickTouchID:(id)sender {
-    [self loadAuthentication];
+    //[self loadAuthentication];
+    [self CheckReceipt:@""];
 }
 /**
  * 指纹登录验证
@@ -164,57 +165,67 @@
                     NSLog(@"%ld", (long)error.code); // 错误码 error.code
                     switch (error.code)
                     {
-                        case LAErrorAuthenticationFailed: // Authentication was not successful, because user failed to provide valid credentials
+                        case LAErrorAuthenticationFailed:
                         {
+                            // Authentication was not successful, because user failed to provide valid credentials
                             [STTextHudTool showErrorText:@"授权失败"]; // -1 连续三次指纹识别错误
                         }
                             break;
-                        case LAErrorUserCancel: // Authentication was canceled by user (e.g. tapped Cancel button)
+                        case LAErrorUserCancel:
                         {
+                            // Authentication was canceled by user (e.g. tapped Cancel button)
                             [STTextHudTool showErrorText:@"用户取消验证Touch ID"]; // -2 在TouchID对话框中点击了取消按钮
                         }
                             break;
-                        case LAErrorUserFallback: // Authentication was canceled, because the user tapped the fallback button (Enter Password)
+                        case LAErrorUserFallback:
                         {
+                            // Authentication was canceled, because the user tapped the fallback button (Enter Password)
                             [[NSOperationQueue mainQueue] addOperationWithBlock:^{
                                 [STTextHudTool showErrorText:@"用户选择输入密码，切换主线程处理"]; // -3 在TouchID对话框中点击了输入密码按钮
                             }];
                         }
                             break;
-                        case LAErrorSystemCancel: // Authentication was canceled by system (e.g. another application went to foreground)
+                        case LAErrorSystemCancel:
                         {
+                            // Authentication was canceled by system (e.g. another application went to foreground)
                             [STTextHudTool showErrorText:@"取消授权，如其他应用切入，用户自主"]; // -4 TouchID对话框被系统取消，例如按下Home或者电源键
                         }
                             break;
-                        case LAErrorPasscodeNotSet: // Authentication could not start, because passcode is not set on the device.
+                        case LAErrorPasscodeNotSet:
                         {
+                            // Authentication could not start, because passcode is not set on the device.
                             [STTextHudTool showErrorText:@"设备系统未设置密码"]; // -5
                         }
                             break;
-                        case LAErrorBiometryNotAvailable: // Authentication could not start, because Touch ID is not available on the device
+                        case LAErrorBiometryNotAvailable:
                         {
+                            // Authentication could not start, because Touch ID is not available on the device
                             [STTextHudTool showErrorText:@"设备未设置Touch ID"]; // -6
                         }
                             break;
-                        case LAErrorBiometryNotEnrolled: // Authentication could not start, because Touch ID has no enrolled fingers
+                        case LAErrorBiometryNotEnrolled:
                         {
+                            // Authentication could not start, because Touch ID has no enrolled fingers
                             [STTextHudTool showErrorText:@"用户未录入指纹"]; // -7
                         }
                             break;
                         
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_9_0
-                        case LAErrorBiometryLockout: //Authentication was not successful, because there were too many failed Touch ID attempts and Touch ID is now locked. Passcode is required to unlock Touch ID, e.g. evaluating LAPolicyDeviceOwnerAuthenticationWithBiometrics will ask for passcode as a prerequisite 用户连续多次进行Touch ID验证失败，Touch ID被锁，需要用户输入密码解锁，先Touch ID验证密码
+                        case LAErrorBiometryLockout:
                         {
+                            //Authentication was not successful, because there were too many failed Touch ID attempts and Touch ID is now locked. Passcode is required to unlock Touch ID, e.g. evaluating LAPolicyDeviceOwnerAuthenticationWithBiometrics will ask for passcode as a prerequisite 用户连续多次进行Touch ID验证失败，Touch ID被锁，需要用户输入密码解锁，先Touch ID验证密码
                             [STTextHudTool showErrorText:@"Touch ID被锁，需要用户输入密码解锁"]; // -8 连续五次指纹识别错误，TouchID功能被锁定，下一次需要输入系统密码
                         }
                             break;
-                        case LAErrorAppCancel: // Authentication was canceled by application (e.g. invalidate was called while authentication was in progress) 如突然来了电话，电话应用进入前台，APP被挂起啦");
+                        case LAErrorAppCancel:
                         {
+                            // Authentication was canceled by application (e.g. invalidate was called while authentication was in progress) 如突然来了电话，电话应用进入前台，APP被挂起啦");
                             [STTextHudTool showErrorText:@"用户不能控制情况下APP被挂起"]; // -9
                         }
                             break;
-                        case LAErrorInvalidContext: // LAContext passed to this call has been previously invalidated.
+                        case LAErrorInvalidContext:
                         {
+                            // LAContext passed to this call has been previously invalidated.
                             [STTextHudTool showErrorText:@"LAContext传递给这个调用之前已经失效"]; // -10
                         }
                             break;
