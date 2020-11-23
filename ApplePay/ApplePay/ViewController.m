8edf7338e70270f3cb651726bfd9aea16bf04bbc
Leo <m5pro@M1-MacPro.local>
2020-11-23 10:43:34 +0800

    Hugo

--- ApplePay/ApplePay/ViewController.m
+++ ApplePay/ApplePay/ViewController.m
@@ -283,48 +283,16 @@
     NSLog(@"取消Facebook分享");
 }
 
-- (void)encodeWithCoder:(nonnull NSCoder *)coder {
-    
-}
-
-- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
-    
-}
-
-- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-    
-}
-
-- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
-    return CGSizeMake(0, 0);
-}
-
-- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-    
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-    
-}
-
-- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-    
-}
-
-- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
-    
-}
-
-- (void)setNeedsFocusUpdate {
-    
-}
-
-- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
-    return NO;
-}
-
-- (void)updateFocusIfNeeded {
-    
-}
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {}
+- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {}
+- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {}
+- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize { return CGSizeMake(0, 0); }
+- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {}
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {}
+- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {}
+- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {}
+- (void)setNeedsFocusUpdate {}
+- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context { return NO; }
+- (void)updateFocusIfNeeded {}
 
 @end
